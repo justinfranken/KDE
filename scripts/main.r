19dc1a8ce77f2397783800f60fcf4703d6185029
Niklas Niedermeier <niedermeier.niklas@web.de>
2023-11-18 19:27:35 +0100

    add seed to main

--- scripts/main.r
+++ scripts/main.r
@@ -1,2 +1,8 @@
 # import libraries
 source(paste0(getwd(),"/scripts/lib.r"))
+
+# import sources
+source(paste0(getwd(),"/scripts/functions/kernel_density_estimation.r"))
+
+# set seed for reproducibility
+set.seed(4322)
