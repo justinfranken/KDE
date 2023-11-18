d91f3092d682946f96e701c29006b7a7ae67d7db
Niklas Niedermeier <niedermeier.niklas@web.de>
2023-11-18 23:09:14 +0100

    Add bandwidth script to main

--- scripts/main.r
+++ scripts/main.r
@@ -3,6 +3,7 @@ source(paste0(getwd(),"/scripts/lib.r"))
 
 # import sources
 source(paste0(getwd(),"/scripts/functions/kernel_density_estimation.r"))
+source(paste0(getwd(),"/scripts/functions/bandwidth_selection.r"))
 
 # set seed for reproducibility
 set.seed(4322)
