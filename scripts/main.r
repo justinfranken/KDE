482732ceb52d572eb58a0815c74b3760349ce5d3
Niklas Niedermeier <niedermeier.niklas@web.de>
2023-12-18 12:00:51 +0100

    Add calculation of coverage prob to main script.

--- scripts/main.r
+++ scripts/main.r
@@ -4,6 +4,30 @@ source(paste0(getwd(),"/scripts/lib.r"))
 # import sources
 source(paste0(getwd(),"/scripts/functions/kernel_density_estimation.r"))
 source(paste0(getwd(),"/scripts/functions/bandwidth_selection.r"))
+source(paste0(getwd(),"/scripts/functions/bias_estimation.r"))
+source(paste0(getwd(),"/scripts/functions/confidence_interval.r"))
+source(paste0(getwd(),"/scripts/functions/coverage_prob.r"))
 
 # set seed for reproducibility
 set.seed(4322)
+
+#-------------------       coverage probability      ---------------------------
+
+n_range <- c(50,100,500,1000)
+
+coverage_vec <- c()
+  
+for (n in n_range){
+  coverage <- coverage_for_n(
+                 n = n,
+                 S = 500, # Simulations
+                 data_model = "m1",
+                 x_point = 0, # evaluation point for ci
+                 bandwidth_model = "plug_in_sj" ,
+                 conf_int_model = "rbc",
+                 kernel = "gaussian",
+                 alpha = 0.05)
+  
+  coverage_vec <- c(coverage_vec,coverage)
+  
+}
