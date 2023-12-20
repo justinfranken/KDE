36c1729f7bbb621723157d02239e9fd295c88331
Niklas Niedermeier <niedermeier.niklas@web.de>
2023-12-20 11:13:12 +0100

    add variance calculation a la calonico

--- scripts/functions/confidence_interval.R
+++ scripts/functions/confidence_interval.R
@@ -10,6 +10,8 @@ conf_int <- function(x,
   
   # n
   n <- length(x)
+  # z
+  z <- qnorm(1-alpha/2, mean = 0, sd = 1)
   
   if (model ==  "bc"){
     
@@ -17,20 +19,18 @@ conf_int <- function(x,
     b = h
     
     # density for x_point
-    f_hat_of_x <- kde_univariate(x_train = x, h = h, kernel = kernel, density_for_x = x_point)$y
+    f_point_hat <- kde_univariate(x_train = x, h = h, kernel = kernel, density_for_x = x_point)$f
     
     # bias corrected density for x_point
-    f_hat_of_x_bc <- kde_unbiased_univariate(x_train = x, h = h, b = b, kernel = kernel, density_for_x = x_point)$y
+    f_point_hat_bc<- kde_unbiased_univariate(x_train = x, h = h, b = b, kernel = kernel, density_for_x = x_point)$f
     
     # estimation of standard deviation
-    f_hat <- kde_univariate(x_train = x, h = h, kernel = kernel)$y
-    sd_hat <- sd(f_hat)
+    var_point_hat_us <- kde_univariate(x_train = x, h = h, kernel = kernel)$var_f_hat
+    sd_point_hat_us <- sqrt(var_point_hat_us)
     
     # estimation of bias corrected confindence intervals
-    z <- qnorm(1-alpha/2, mean = 0, sd = 1)
-    ci_lower <- f_hat_of_x_bc - z * (sd_hat / sqrt(n*h))
-    ci_upper <- f_hat_of_x_bc + z * (sd_hat / sqrt(n*h))
-    ci <- c(ci_lower, ci_upper)
+    ci_lower <- f_point_hat_bc - z * (sd_point_hat_us  / sqrt(n*h))
+    ci_upper <- f_point_hat_bc + z * (sd_point_hat_us  / sqrt(n*h))
   }
  
   if (model ==  "rbc"){
@@ -39,38 +39,35 @@ conf_int <- function(x,
     b = h
     
     # density for x_point
-    f_hat_of_x <- kde_univariate(x_train = x, h = h, kernel = kernel, density_for_x = x_point)$y
+    f_point_hat <- kde_univariate(x_train = x, h = h, kernel = kernel, density_for_x = x_point)$f
     
     # bias corrected density for x_point
-    f_hat_of_x_rbc <- kde_unbiased_univariate(x_train = x, h = h, b = b, kernel = kernel, density_for_x = x_point)$y
+    f_point_hat_rbc <- kde_unbiased_univariate(x_train = x, h = h, b = b, kernel = kernel, density_for_x = x_point)$f
     
     # estimation of standard deviation to get robust estimate
-    f_hat_rbc <- kde_unbiased_univariate(x_train = x, h = h, b = b, kernel = kernel)$y
-    sd_hat <- sd(f_hat_rbc)
+    var_point_hat_rbc <- kde_unbiased_univariate(x_train = x, h = h, b = b, kernel = kernel)$var_f_hat
+    sd_point_hat_rbc <- sqrt(var_point_hat_rbc)
     
     # estimation of robust bias corrected confindence intervals
-    z <- qnorm(1-alpha/2, mean = 0, sd = 1)
-    ci_lower <- f_hat_of_x_rbc - z * (sd_hat / sqrt(n*h))
-    ci_upper <- f_hat_of_x_rbc + z * (sd_hat / sqrt(n*h))
-    ci <- c(ci_lower, ci_upper)
+    ci_lower <- f_point_hat_rbc  - z * (sd_point_hat_rbc / sqrt(n*h))
+    ci_upper <- f_point_hat_rbc  + z * (sd_point_hat_rbc / sqrt(n*h))
   }
   
   if (model ==  "us"){
 
     # density for x_point
-    f_hat_of_x <- kde_univariate(x_train = x, h = h, kernel = kernel, density_for_x = x_point)$y
+    f_point_hat <- kde_univariate(x_train = x, h = h, kernel = kernel, density_for_x = x_point)$f
     
     # estimation of standard deviation
-    f_hat <- kde_univariate(x_train = x, h = h, kernel = kernel)$y
-    sd_hat <- sd(f_hat)
+    var_point_hat_us <- kde_univariate(x_train = x, h = h, kernel = kernel, density_for_x = x_point)$var_f_hat
+    sd_point_hat_us <- sqrt(var_point_hat_us)
     
     # estimation of confindence intervals
-    z <- qnorm(1-alpha/2, mean = 0, sd = 1)
-    ci_lower <- f_hat_of_x - z * (sd_hat / sqrt(n*h))
-    ci_upper <- f_hat_of_x + z * (sd_hat / sqrt(n*h))
-    ci <- c(ci_lower, ci_upper)
+    ci_lower <- f_point_hat - z * (sd_point_hat_us / sqrt(n*h))
+    ci_upper <- f_point_hat + z * (sd_point_hat_us / sqrt(n*h))
   }
+
+  ci <- c(ci_lower, ci_upper)
   
-  #return bias corrected confidence interval
   return(ci)
 }
