f5e785eb529c0b6a3dfdfe8d7a2ed1ae8b01087e
Niklas Niedermeier <niedermeier.niklas@web.de>
2023-12-25 15:54:12 +0100

    bug fix

--- scripts/functions/kernel.R
+++ scripts/functions/kernel.R
@@ -17,11 +17,11 @@ gaussian_kernel_second_derivative <- function(v){
 }
 
 epanechnikov_kernel <- function(v){
-  y <- (3/4)*(1-v^2) * ifelse(abs(v)<=1,1,0)
+  y <- (abs(v)<=1) * (3/4)*(1-v^2) 
   return(y)
 }
 
 epanechnikov_kernel_second_derivative <- function(v){
-  y <- -(3/2) * ifelse(abs(v)<=1,1,0)
+  y <- (abs(v)<=1)*15*(3*v^2-1)/4
   return(y)
 }
