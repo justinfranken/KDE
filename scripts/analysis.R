c57d43f1587c508d240527ba62b77de9359a13f2
Niklas Niedermeier <niedermeier.niklas@web.de>
2024-01-06 16:59:55 +0100

    add documentation of analysis

--- scripts/analysis.R
+++ scripts/analysis.R
@@ -24,44 +24,52 @@ coverage_prob_n_plot(data = coverage_prob_grid,
                      x_axis_log = FALSE
 )
 
-# Robust Bias correction with different bandwidth estimators
+#--------------   RBC
 coverage_prob_n_plot_all_points(data = coverage_prob_grid,
                                 data_model = "m1",
                                 conf_int_model = c("rbc"),
                                 bandwidth_model = c("plug_in_sj", "cv","silverman","scott"),
                                 kernel = "epanechnikov"
 )
-# -> Three observations
-#      (1) Evaluation point effects coverage probability
-#      (2) Coverage probability at 95% for higher n
-#      (3) Bandwidth estimators play a minor role (for coverage probability, 
-#          but probably for interval length)
 
-# Undersmoothing with different bandwidth estimators
+# -> General observations independent of confidence model
+#      (1) Evaluation point effects coverage probability
+#      (2) Coverage probability depends on n 
+
+# -> One main observation
+#      (1) Bandwidth estimators play a minor role (for coverage probability, 
+#          but probably for interval length)
+#            
+
+#--------------   US
 coverage_prob_n_plot_all_points(data = coverage_prob_grid,
                                 data_model = "m1",
                                 conf_int_model = c("us"),
                                 bandwidth_model = c("hall_0.3","hall_0.5","hall_0.7"),
                                 kernel = "epanechnikov"
 )
-# -> Two central observation
-#      (1) Evaluation point effects coverage probability
-#      (2) Coverage probability at 95% for higher n
-#      (3) Bandwidth estimators play a larger role 
+# -> One main observation
+#      (1) Bandwidth estimators play a larger role 
+#             - Lowering lambda can reduce bias but increases the variance of f_k
+#               especially if n is small
+#      (2) General Problem: How do choose lambda in practice? Since true distribution
+#          is not available?
+#             - For Model 1-4, we see a faster convergence for lambda = 0.7
 
-# Bias correction with different bandwidth estimators
+#--------------   BC 
 coverage_prob_n_plot_all_points(data = coverage_prob_grid,
                                 data_model = "m1",
                                 conf_int_model = c("bc"),
                                 bandwidth_model = c("plug_in_sj", "cv","silverman","scott"),
                                 kernel = "epanechnikov"
 )
-# -> One central observation
-#      (1) We have no valid construction of confidence interval's,
-#          since rho does not go to zero
 
+# -> One main observation
+#      (1) We have no valid construction of confidence interval's.
+#          Reason: Since rho does not go to zero, we don't account for the variance
+#          of the bias 
 
-# Comparing best combinations
+#--------------   Best models
 coverage_prob_n_plot_all_points(data = coverage_prob_grid,
                                 data_model = "m1",
                                 conf_int_model = c("bc","rbc","us"),
@@ -71,31 +79,34 @@ coverage_prob_n_plot_all_points(data = coverage_prob_grid,
                                 kernel = "epanechnikov"
 )
 
+# -> One main observation
+#      (1) Convergence of rbc seems to be faster in comparison to us
+
 #-------------------         interval length         ---------------------------
 
-# Robust Bias correction with different bandwidth estimators
+#--------------   RBC
 interval_length_n_plot_all_points(data = coverage_prob_grid,
                                 data_model = "m1",
                                 conf_int_model = c("rbc"),
                                 bandwidth_model = c("plug_in_sj", "cv","silverman","scott"),
                                 kernel = "epanechnikov"
 )
-# -> One central observation
-#      (1) Scott is superior
+# -> One cmain observation
+#      (1) Scott is superior for Model 1-4
 
-# undersmoothing 
+#--------------   US
 interval_length_n_plot_all_points(data = coverage_prob_grid,
                                 data_model = "m1",
                                 conf_int_model = c("us"),
                                 bandwidth_model = c("hall_0.3","hall_0.5","hall_0.7"),
                                 kernel = "epanechnikov"
 )
-# -> One central observation
-#      (1) Hall_0.7 is superior
+# -> One main observation
+#      (1) Hall_0.7 is superior for Model 1-4
 #          Explanation: a higher bandwidth leads to a lower variance
 
 
-# Comparing best combinations
+#--------------   Best models
 interval_length_n_plot_all_points(data = coverage_prob_grid,
                                 data_model = "m1",
                                 conf_int_model = c("rbc","us"),
@@ -104,7 +115,6 @@ interval_length_n_plot_all_points(data = coverage_prob_grid,
 )
 
 # -> One central observation
-#      (1) Undersmoothing is better in terms of inter length
-#          Explanation: RBC Standard Errors > US Standard Error, because
-#          RBC Standard Error carry variance of Bias: 
+#      (1) Undersmoothing is better in terms of interval length.
+#          Explanation: RBC Standard Error carry variance of Bias: 
 #          Var(bias corrected estimate) = Var(f-Bias) = Var(f) + Var(Bias) - 2*Cov(f,Bias)
